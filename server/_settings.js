b4660e3dacc4aeb44daafde13b6dbbb7d6391974
Gal Dubitski <gal@tocarta.es>
2016-05-13 10:14:26 +0100

    settings

--- server/_settings.js
+++ server/_settings.js
@@ -9,20 +9,20 @@ _.defaults(Meteor.settings, {
     virtualTrading: true,
     updateMarketInterval: 500,
     testBot2: {
-      appKey: "8GM3PavYlsoCq5Pm",
-      username: "guli@tocarta.es",
-      password: "guliguli1"
+      appKey: "---",
+      username: "---",
+      password: "---"
     }
   },
   
   // twitter settings
   twitter: {
-    consumerKey: "PLfrg2bUh0oL0asi3R2fumRjm", 
-    secret: "sRI8rnwO3sx7xUAxNWTX0WEDWph3WEBHu6tTdJYQ5wVrJeVCCt"
+    consumerKey: "---", 
+    secret: "---"
   },
 
   mongolab: {
-  	apiKey: "wGtHOle0k7O745R7Z_7Emzr0bNGcDIb2"
+    apiKey: "---"
   }
 
 });
